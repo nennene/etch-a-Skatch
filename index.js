1bdb209f76063e24ab7e25e4a10c1071249f5f85
elene qardava <eleqardava@gmail.com>
2025-01-25 16:57:02 +0400

    crated 16*16 squares

---
+++ index.js
@@ -0,0 +1,15 @@
+for(x=0; x<256;x++) {
+var div = document.createElement("div");
+div.style.width = "50px";
+div.style.height = "50px";
+div.style.background = "pink";
+div.style.color = "white";
+div.style.border ="solid, black"
+document.getElementById("main").appendChild(div);
+
+};
+
+
+
+
+
